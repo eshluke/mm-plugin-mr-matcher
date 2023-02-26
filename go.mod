de78dd2fecddb2f96aff028f0bea29ebc52d0bcd
rhoeunju <ej.rho35@gmail.com>
2023-02-26 22:03:25 +0900

    added request body format validation

--- go.mod
+++ go.mod
@@ -8,16 +8,17 @@ require (
 )
 
 require (
-	github.com/BurntSushi/toml v1.2.1 // indirect
-	github.com/google/uuid v1.0.0 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/nicksnyder/go-i18n v1.10.1 // indirect
 	github.com/pborman/uuid v1.2.1 // indirect
-	github.com/pelletier/go-toml v1.2.0 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/atomic v1.8.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
--- plugin.go
+++ plugin.go
@@ -7,13 +7,24 @@ import (
 
 	"com.github.eshluke.helloworld/gitlab"
 	"com.github.eshluke.helloworld/helper"
+	"github.com/mattermost/mattermost-server/plugin"
 	"github.com/mattermost/mattermost-server/plugin/rpcplugin"
 )
 
-type Plugin struct{}
+type Plugin struct {
+	api plugin.API
+}
 
 func (p *Plugin) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!")
+	switch path := r.URL.Path; path {
+	case "/gitlab/mr":
+		p.handleMergeRequest(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func (p *Plugin) handleMergeRequest(w http.ResponseWriter, r *http.Request) {
 	var mr gitlab.MergeRequest
 	err := helper.DecodeJSONBody(w, r, &mr)
 	if err != nil {
@@ -25,9 +36,15 @@ func (p *Plugin) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	if mr.ObjectKind == "merge_request" {
-		fmt.Fprintf(w, "merge_request event received!!!")
+	if mr.ObjectKind == "merge_request" &&
+		(mr.ObjectAttributes.Action == "open" || mr.ObjectAttributes.Action == "reopen") &&
+		len(mr.Reviewers) == 0 {
+		fmt.Fprintf(w, "TODO: handle merge request event...")
 	}
+}
+
+func (p *Plugin) createPost(mr *gitlab.MergeRequest) {
+
 }
 
 // This example demonstrates a plugin that handles HTTP requests which respond by greeting the
